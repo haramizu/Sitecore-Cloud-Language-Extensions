4d3539b65d71730a6ce8fe51517e9e53e2077754
Shinichi Haramizu <shinichi@haramizu.jp>
2024-12-24 17:26:09 +0900

    add multi language

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,30 +1,50 @@
 import en from './resources/en.json';
-import ja from './resources/ja.json';
+import languages from './resources/languages.json';
 
 type TranslationKeys = keyof typeof en;
 
 export default defineContentScript({
   matches: ['*://*.sitecorecloud.io/*'],
   main() {
+    const lang = navigator.language.slice(0, 2);
+
+    console.log('lang: ' + lang);
+
+    // If the language is 'en', do not execute
+    if (lang.startsWith('en') || !Object.keys(languages).some(language => lang.startsWith(language))) {
+      console.log('Original');
+
+      return;
+    }
+
     // Replace text in HTML
-    replaceTextInSelector();
+    replaceTextInSelector(lang);
 
     // Observe changes in the DOM
     const observer = new MutationObserver(() => {
-      replaceTextInSelector();
+      replaceTextInSelector(lang);
     });
     observer.observe(document.body, { childList: true, subtree: true });
   },
 });
 
-function replaceTextInSelector() {
+async function replaceTextInSelector(lang: string) {
   const selectors = Object.keys(en) as TranslationKeys[];
+  let translations: Record<string, string> | null = null;
+
+  try {
+    const module = await import(`./resources/${lang}.json`);
+    translations = module.default;
+  } catch (error) {
+    console.error(`Translation file for ${lang} not found.`);
+    return;
+  }
 
   selectors.forEach(selector => {
     const element = document.querySelector(selector);
-    if (element?.textContent?.trim() === en[selector]) {
-      if (ja[selector as keyof typeof ja]) {
-        element.textContent = ja[selector as keyof typeof ja];
+    if (translations && element?.textContent?.trim() === en[selector]) {
+      if (translations[selector as keyof typeof translations]) {
+        element.textContent = translations[selector as keyof typeof translations];
       }
     }
   });
