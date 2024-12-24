b6816c3a6f95ca84bda9386883e94bcb69ab23c6
Shinichi Haramizu <shinichi@haramizu.jp>
2024-12-24 14:57:39 +0900

    update resources

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,3 +1,8 @@
+import en from './resources/en.json';
+import ja from './resources/ja.json';
+
+type TranslationKeys = keyof typeof en;
+
 export default defineContentScript({
   matches: ['*://*.sitecorecloud.io/*'],
   main() {
@@ -15,24 +20,12 @@ export default defineContentScript({
 });
 
 function replaceTextInSelector() {
-  // Replace text in header
-  const selector = 'body > form > header';
-  const element = document.querySelector(selector);
-  if (element?.textContent?.trim() === "Select organization") {
-    element.textContent = "組織の選択";
-  }
+  const selectors = Object.keys(en) as TranslationKeys[];
 
-  // Replace text in footer link
-  const footerLinkSelector = 'body > form > footer > a';
-  const footerLinkElement = document.querySelector(footerLinkSelector);
-  if (footerLinkElement?.textContent?.trim() === "Cancel") {
-    footerLinkElement.textContent = "取り消し";
-  }
-
-  // Replace text in Quick Link
-  const QuickLinkSelector = 'body > sitecore-region > sitecore-extension > div.css-6rjclo > div > div > div.css-1ha1tj3 > div > div > h2';
-  const QuickLinkElement = document.querySelector(QuickLinkSelector);
-  if (QuickLinkElement?.textContent?.trim() === "Quick Links") {
-    QuickLinkElement.textContent = "クイックリンク";
-  }
+  selectors.forEach(selector => {
+    const element = document.querySelector(selector);
+    if (element?.textContent?.trim() === en[selector as TranslationKeys]) {
+      element.textContent = ja[selector as TranslationKeys];
+    }
+  });
 }
