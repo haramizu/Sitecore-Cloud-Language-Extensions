7ad1e18786f9efb935b58ad5ee985cb00a2eaef0
Shinichi Haramizu <shinichi@haramizu.jp>
2024-12-25 14:33:42 +0900

    update domain resource

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,39 +1,47 @@
-import en from './resources/en.json';
 import languages from './resources/languages.json';
 
-type TranslationKeys = keyof typeof en;
+type TranslationKeys = keyof typeof String;
 
 export default defineContentScript({
   matches: ['*://*.sitecorecloud.io/*'],
   main() {
     const lang = navigator.language.slice(0, 2);
 
-    console.log('lang: ' + lang);
+    const domain = window.location.hostname;
+    console.log('domain: ' + domain);
 
     // If the language is 'en', do not execute
     if (lang.startsWith('en') || !Object.keys(languages).some(language => lang.startsWith(language))) {
-      console.log('Original');
-
       return;
     }
 
     // Replace text in HTML
-    replaceTextInSelector(lang);
+    replaceTextInSelector(lang, domain);
 
     // Observe changes in the DOM
     const observer = new MutationObserver(() => {
-      replaceTextInSelector(lang);
+      replaceTextInSelector(lang, domain);
     });
     observer.observe(document.body, { childList: true, subtree: true });
   },
 });
 
-async function replaceTextInSelector(lang: string) {
-  const selectors = Object.keys(en) as TranslationKeys[];
+async function replaceTextInSelector(lang: string, domain: string) {
+  let selectors: TranslationKeys[] = [];
+  let en: Record<string, string>;
+
+  try {
+    en = await import(`./resources/${domain}/en.json`).then(module => module.default).catch(() => null);
+    selectors = Object.keys(en) as TranslationKeys[];
+  } catch (error) {
+    console.error(`English translation file for ${domain} not found.`);
+    return;
+  }
+
   let translations: Record<string, string> | null = null;
 
   try {
-    const module = await import(`./resources/${lang}.json`);
+    const module = await import(`./resources/${domain}/${lang}.json`);
     translations = module.default;
   } catch (error) {
     console.error(`Translation file for ${lang} not found.`);
